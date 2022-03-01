31c141eba610b59b50b878dbf9f83d8c86a07f38
signan <signmeup132@gmail.com>
2022-03-01 22:35:21 +0300

    add function to add 'hover' class

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ function createGrid() {
     for (let i = 0; i < 256; i++) {
         let div = document.createElement('div');
         div.classList.add('pixel');
+        div.addEventListener("mouseover", () => addHoverClass(div));
         div.textContent = `${i+1}`;
         container.appendChild(div);
     }
 }
+
+function addHoverClass(element) {
+    element.classList.add('hover');
+}
+
+createGrid()
