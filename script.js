ff4f03a4718c85eb2cc3e7857b98bec2b2b75706
signan <signmeup132@gmail.com>
2022-03-04 17:16:02 +0300

    refactor createGrid() to accept any size, add function to reset button

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const container = document.querySelector('.container');
+const resetBtn = document.querySelector('.reset-btn');
 
-function createGrid() {
-    for (let i = 0; i < 256; i++) {
-        let div = document.createElement('div');
-        div.classList.add('pixel');
-        div.addEventListener("mouseover", () => addHoverClass(div));
-        div.textContent = `${i+1}`;
-        container.appendChild(div);
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        let rowContainer = document.createElement('div');
+        rowContainer.classList.add('row');
+        container.appendChild(rowContainer);
+
+        for (j = 0; j < gridSize; j++) {
+            let div = document.createElement('div');
+            div.classList.add('pixel');
+            div.addEventListener("mouseover", addHoverClass);
+            rowContainer.appendChild(div);
+        }
     }
 }
 
-function addHoverClass(element) {
-    element.classList.add('hover');
+function addHoverClass(event) {
+    event.currentTarget.classList.add('hover');
 }
 
-createGrid()
+function removeDivs() {
+    divs = document.querySelectorAll('.row')
+    for (let i = 0; i < divs.length; i++) {
+        container.removeChild(divs.item(i));
+    }
+}
+
+function resetBoard() {
+    gridSize = prompt('what size of gird do you want?');
+    if (gridSize > 100) {
+        gridSize = 100;
+    }
+    removeDivs();
+    createGrid(gridSize);
+}
+
+
+resetBtn.addEventListener("click", resetBoard);
+
+createGrid(50);
